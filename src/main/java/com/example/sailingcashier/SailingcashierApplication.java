b525055321aecd3235bef6ad0f3cf90559050dff
Theodor Grip <theo.grip@googlemail.com>
2021-01-23 10:46:11 +0100

    Initial commit

---
+++ src/main/java/com/example/sailingcashier/SailingcashierApplication.java
@@ -0,0 +1,13 @@
+package com.example.sailingcashier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailingcashierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailingcashierApplication.class, args);
+    }
+
+}
