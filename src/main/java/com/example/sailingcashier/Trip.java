76aee4da73ddf5bd4e0de006347c322064bf4a2a
Theodor Grip <theo.grip@googlemail.com>
2021-01-23 13:59:32 +0100

    start modeling trip

--- src/main/java/com/example/sailingcashier/Trip.java
+++ src/main/java/com/example/sailingcashier/Trip.java
@@ -1,4 +1,15 @@
 package com.example.sailingcashier;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Trip {
+
+    private LocalDate start;
+    private LocalDate end;
+
+    public Trip(LocalDate start, LocalDate end) {
+        this.start = start;
+        this.end = end;
+    }
 }
---
+++ src/test/java/com/example/sailingcashier/TripServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.sailingcashier;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripServiceTest {
+
+    @Test
+    void addTrip() {
+        LocalDate start = LocalDate.of(2021, Month.APRIL, 16);
+        LocalDate finish = LocalDate.of(2021, Month.MAY, 1);
+    }
+}
